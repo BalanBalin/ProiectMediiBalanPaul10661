38035be333b7f7feccfc0673826d141c9708425e
BalanBalin <balanpaul751@gmail.com>
2025-01-31 23:55:06 +0200

    Modificari finale

--- ProiectMediiBalanPaul10661/Models/Programare.cs
+++ ProiectMediiBalanPaul10661/Models/Programare.cs
@@ -4,14 +4,14 @@ namespace ProiectMediiBalanPaul10661.Models
 {
     public class Programare
     {
-        public int Id { get; set; } // Primary Key
+        public int Id { get; set; }
 
-        public string Pacient { get; set; } // Store Patient Name as a String
+        public string Pacient { get; set; }
 
-        public string Doctor { get; set; } // Store Doctor Name as a String
+        public string Doctor { get; set; }
 
-        public string Serviciu { get; set; } // Store Service Name as a String
+        public string Serviciu { get; set; }
 
-        public DateTime DataProgramare { get; set; } // Date of Appointment
+        public DateTime DataProgramare { get; set; }
     }
 }
